8c78bc7a62404dabaaa22697e0f7393b91488827
krack3 <mike@krackeonline.com>
2017-03-16 09:56:02 -0500

    functions.php - added theme setup function

--- functions.php
+++ functions.php
@@ -1,9 +1,24 @@
 <?php
 
-/*** Child Theme Function  ***/
 
-function malmo_elated_child_theme_enqueue_scripts() {
-	wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
-	wp_enqueue_style( 'childstyle' );
-}
-add_action('wp_enqueue_scripts', 'malmo_elated_child_theme_enqueue_scripts', 11);
+// Check to see if setup function already exists
+if ( !function_exists( 'malmo_child_nep_theme_setup' ) ) {
+
+	function malmo_child_nep_theme_setup() {
+
+		/**
+		*    Child Theme Function
+		*/
+
+		// Register and enqueue the parent stylesheet
+		function malmo_elated_child_theme_enqueue_scripts() {
+			wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+			wp_enqueue_style( 'childstyle' );
+		}
+		add_action('wp_enqueue_scripts', 'malmo_elated_child_theme_enqueue_scripts', 11);
+
+
+	}  // End malmo_child_nep_theme_setup()
+	add_action('after_setup_theme', 'malmo_child_nep_theme_setup');
+
+} // End if ( !function_exists( 'malmo_child_nep_theme_setup' ) )
